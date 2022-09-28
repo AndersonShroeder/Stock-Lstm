b35d013786973be5a514c6c8eda9b9fe67e07985
Tigiy98 <anderson.schroeder0317@gmail.com>
2022-09-28 16:45:10 -0500

    helper

--- Short Run Prediction/helper.py
+++ Short Run Prediction/helper.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import datetime
 import numpy as np
 
-def plot_x_days_stock(x, dataframe, subplot = plt):
+def plot_x_days_stock(x, subreddit, dataframe, subplot = plt):
     lst = []
     times = []
     for i in range(1, x + 1):
@@ -14,18 +14,19 @@ def plot_x_days_stock(x, dataframe, subplot = plt):
         lst += dataframe.iloc[i]['price']
     subplot.plot(times, lst)
 
-def plot_x_days_sentiment(x, dataframe, data_gather, subplot = plt):
+def plot_x_days_sentiment(x, subreddit, dataframe, data_gather, subplot = plt):
     dct_lst = {}
+    print('ruh')
     for point in data_gather.keys():
         dct_lst[f'{point}'] = np.array(
-            [dataframe.iloc[i]['Economics'][key][point] for i in range(x) for key in dataframe.iloc[i]['Economics']]).astype(float)
+            [dataframe.iloc[i][subreddit][key][point] for i in range(x) for key in dataframe.iloc[i][subreddit]]).astype(float)
         
         dct_lst[f'{point}'][f'{point}_list'==0] = np.nan
 
     y_vals = []
     for i in range(x):
         date = dataframe.index[i]
-        for key in dataframe.iloc[i]['Economics']:
+        for key in dataframe.iloc[i][subreddit]:
             time = key.split(":")
             y_vals.append(datetime.datetime(date.year,date.month,date.day,int(time[0]),int(time[1])))
 
@@ -33,7 +34,7 @@ def plot_x_days_sentiment(x, dataframe, data_gather, subplot = plt):
         subplot.bar(y_vals, list, color = data_gather[key], width=.005)
         subplot.bar(y_vals, list, color = data_gather[key], width=.005)
 
-def plot_x_days(x, sentiment_dataframe, stock_dataframe):
+def plot_x_days(x, subreddit, sentiment_dataframe, stock_dataframe):
     figure, axis = plt.subplots(2,1)
-    plot_x_days_sentiment(x, sentiment_dataframe, {'neg': 'red','pos':'green'},axis[0])
-    plot_x_days_stock(x, stock_dataframe, axis[1])
+    plot_x_days_sentiment(x, subreddit, sentiment_dataframe, {'neg': 'red','pos':'green'},axis[0])
+    plot_x_days_stock(x, subreddit, stock_dataframe, axis[1])
